c9f297bd692737173fcacb5b696814197871a0f2
Matheus Ferreira Amaral <168740667+mateto2@users.noreply.github.com>
2026-06-28 17:41:49 -0300

    Add getters and setters , override for encaminhamento property

--- src/models/ClinicoGeral.java
+++ src/models/ClinicoGeral.java
@@ -19,3 +19,31 @@ public class ClinicoGeral extends Profissional {
         super(nome, cpf, "clinica geral", registroProfissional, valorConsulta);
         setEncaminhamento(encaminhamento);
     }
+// ========== GETTERS E SETTERS ==========
+    
+    public String getEncaminhamento() {
+        return encaminhamento;
+    }
+    
+    public void setEncaminhamento(String encaminhamento) {
+        this.encaminhamento = (encaminhamento != null) ? encaminhamento : "Nenhum";
+    }
+    
+    // ========== SOBRESCRITA ==========
+    
+    @Override
+    public String exibirResumo() {
+        return super.exibirResumo() + 
+               " | Encaminhamento: " + encaminhamento + 
+               " [CLINICO GERAL]";
+    }
+    
+    @Override
+    public String registrarEspecifico(Atendimento atendimento) {
+        if (!encaminhamento.equals("Nenhum")) {
+            atendimento.adicionarInfoEspecialidade("ENCAMINHAMENTO: " + encaminhamento);
+            return "Paciente encaminhado para: " + encaminhamento;
+        }
+        return "Nenhum encaminhamento necessário.";
+    }
+}
